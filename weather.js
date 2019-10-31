ca40acdf48eaaf5d89546d4fa86977248a60a79c
David Vega <dvega920@gmail.com>
2019-10-31 19:12:21 -0400

    added APIKey and ajax call to open weather

--- weather.js
+++ weather.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
+    var zipCode = "02451";
 
+    var url = `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&APPID=${APIKey}`;
 
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
     // END OF CODE
 })
