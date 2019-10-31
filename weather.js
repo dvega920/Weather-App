1da21ad766311dbcde895315973cbf4b1fd69fdb
David Vega <dvega920@gmail.com>
2019-10-30 20:34:54 -0400

    successfull call to weather API

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+
+    //search feature
+    var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
+
+    var query = "http://api.openweathermap.org/data/2.5/forecast?q=haverhill&APPID=" + APIKey;
+
+
+    //ajax call 
+    $.ajax({
+        url: query,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    })
 
 
     // END OF CODE
