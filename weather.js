fe3b64440458beb7f5688cbde4fef4ba9af9e4d2
David Vega <dvega920@gmail.com>
2019-10-31 21:43:29 -0400

    added zip to local storage, printed city name

--- weather.js
+++ weather.js
@@ -1,15 +1,40 @@
 $(document).ready(function () {
 
-    var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
-    var zipCode = "02451";
+    var currentDate = moment().format("MM/DD/YYYY");
+    console.log(currentDate);
 
-    var url = `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&APPID=${APIKey}`;
 
-    $.ajax({
-        url: url,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    $("button").on("click", function (event) {
+        event.preventDefault();
+
+
+        var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
+        var zipCode = $(".search-box").val();
+        // console.log(zipCode);
+        var value = $(".search-box").val();
+        localStorage.setItem("zip", value);
+
+        var getZip = localStorage.getItem("zip");
+        $("div ul li").text(getZip);
+
+        var url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=${APIKey}`
+
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            $(".city-name").text(response.name);
+            $("span").text(currentDate);
+        })
+
+
+
+
     })
+
+
+
+
     // END OF CODE
 })
