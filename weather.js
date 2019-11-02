3caa9508641f167477f3ffc83c8c63b82034bd8e
David Vega <dvega920@gmail.com>
2019-11-02 17:09:43 -0400

    deleted code garbage, set/get city name to/from local storage, output to page. persistance working until page refresh.

--- weather.js
+++ weather.js
@@ -1,37 +1,41 @@
 $(document).ready(function () {
 
     var currentDate = moment().format("MM/DD/YYYY");
-    console.log(currentDate);
+    // console.log(currentDate);
 
     $("button").on("click", function (event) {
         event.preventDefault();
 
-
         var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
         var zipCode = $(".search-box").val();
-        // console.log(zipCode);
-        var value = $(".search-box").val();
-        localStorage.setItem("zip", value);
-
-        var getZip = localStorage.getItem("zip");
-        $("div ul li").text(getZip);
-
+        // var cityName = [];
         var url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=${APIKey}`
+
+        localStorage.setItem("zip", zipCode);
 
         $.ajax({
             url: url,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            $(".city-name").html(response.name + " (" + currentDate + ")" );
+            var cityName = response.name;
+            // console.log(cityName);
+
+            localStorage.setItem("city", cityName);
+
+            var city = localStorage.getItem("city");
+            var li = $("<li>");
+
+            $(".list-group").append(li)
+            li.text(city);
+
+            $(".city-name").html(response.name + " (" + currentDate + ")");
             $(".temp").html("Temperature: " + response.main.temp + " &#176" + "F");
             $(".humidity").text("Humidity: " + response.main.humidity + "%");
             $(".wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
-            $(".uv-index").text("UV Index: " + response.main.temp);
+            $(".uv-index").text("UV Index: " + "empty");
 
         })
-
-
 
 
     })
