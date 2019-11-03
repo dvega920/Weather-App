2417689e82420af7b0bef515cdbf46659904e20c
David Vega <dvega920@gmail.com>
2019-11-03 07:37:09 -0500

    refactored code, added conditionals, function to get the data from the API

--- weather.js
+++ weather.js
@@ -1,49 +1,106 @@
 $(document).ready(function () {
-
     var currentDate = moment().format("MM/DD/YYYY");
-    // console.log(currentDate);
+    var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
 
-    // for (var i = 0; i < localStorage.length; i++) {
-    //     var cityName = localStorage.getItem(i);
-    //     console.log(cityName);
-    // }
+    // pulls city names from local storage (if any) and creates an empty array if nothing is in ls
+    var cityNames = JSON.parse(localStorage.getItem("city"));
+
+    if (cityNames === null) {
+        citiesArray = [];
+    }
+
+    // need to add a forEach to loop through citiesArray to check for saved values
 
     $("button").on("click", function (event) {
         event.preventDefault();
 
-        var APIKey = "f41986cb305bbebfdd6bfe2ffeb29d4a";
-        var city = $(".search-box").val();
-        var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}&units=imperial`
+
+        var search = $(".search-box").val();
+        // console.log(search);
+        cityNames = JSON.parse(localStorage.getItem("cityNames"));
+
+
+        if (search == "" || search == null) {
+            return;
+        }
+
+
+        search.val = "";
+
+        if (cityNames === null) {
+            citiesArray = [search];
+        } else {
+            citiesArray.push(search);
+        }
+
+        console.log(citiesArray);
+        cityData(search);
+
+        localStorage.setItem("cityNames", JSON.stringify(citiesArray));
+
+    })
+
+
+    function cityData(search) {
+
+        var cityName = $("<h1>");
+        var temp = $("<p>");
+        var humidity = $("<p>");
+        var windSpeed = $("<p>");
+        var uvIndex = $("<p>");
+        var url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=${APIKey}&units=imperial`
 
         $.ajax({
             url: url,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            var cityName = response.name;
-            // console.log(cityName);
+            cityName.text(response.name + " (" + currentDate + ")");
+            // var cityName = response.name;
+            temp.html("Temperature: " + response.main.temp + " &#176" + "F");
+            humidity.text("Humidity: " + response.main.humidity + "%");
+            windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
+            uvIndex.text("UV Index: " + "empty");
 
-            localStorage.setItem("city", cityName);
+            //Need to find a weather icon from font awesome and append to cityName
 
-            var city = localStorage.getItem("city");
-            var li = $("<li>");
+            var longitute;
+            var latitude;
 
-            $(".list-group").append(li)
-            li.text(city);
+            // api url to get the uv index
+            var uvURL;
 
-            $(".city-name").html(response.name + " (" + currentDate + ")");
-            $(".temp").html("Temperature: " + response.main.temp + " &#176" + "F");
-            $(".humidity").text("Humidity: " + response.main.humidity + "%");
-            $(".wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
-            $(".uv-index").text("UV Index: " + "empty");
+            //separate ajax call to get the uv index and append to uvIndex variable
+            // $.ajax({
+            //     url: url,
+            //         method: "GET"
+            // }).then(function (response) {
+
+            // })
+
+            $("div.card-body").append(cityName);
+            $("div.card-body").append(temp);
+            $("div.card-body").append(humidity);
+            $("div.card-body").append(windSpeed);
+            $("div.card-body").append(uvIndex);
 
         })
 
 
-    })
+
+
+
+    }
 
 
 
 
-    // END OF CODE
-})
+})    // END OF $(document).ready() 
+
+
+
+
+
+
+
+
